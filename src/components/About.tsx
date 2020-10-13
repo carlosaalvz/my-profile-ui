d722d6106840285c1dff4f03739cb5e438142bb5
carlosalvz <carlosalvarez@carloss-mbp.lan>
2020-10-13 10:44:51 -0500

    minor changes to the profile

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -16,11 +16,11 @@ const About: React.FC = () => {
                 <section id="about" className="about-container">
                     <CSSTransition in={show} classNames="fade" timeout={3000}>
                         <div > 
-                            <h1>Hey, my name is</h1>
+                            <h1>Hello, my name is</h1>
                             <h2>Carlos Alvarez.</h2>
                             <h3>I build software systems and ocassionaly random stuff like this.</h3>
                             <div className="description">
-                                I'm a software engineer based out of Dallas-Fort Worth TX specializing in building scaleable applications.
+                                I'm a software engineer based out of Dallas-Fort Worth, TX specializing in building scaleable applications.
                             </div>
                         </div>
                     </CSSTransition>
