dedbb5b52fa5e5a3e7d735798568e67c7c6e0129
carlosalvz <carlosalvarez@carloss-mbp.lan>
2020-10-15 13:30:16 -0500

    removed auth config

--- src/service.tsx
+++ src/service.tsx
@@ -2,6 +2,5 @@ import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
 
 export function configureAmplify() {
-  Auth.configure(awsconfig);
   Amplify.configure(awsconfig);
 } 
